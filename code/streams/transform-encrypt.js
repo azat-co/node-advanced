4b7a21baacd455e9065b5d979440b8975964666e
Azat Mardan <hi@azat.co>
2018-01-14 19:55:02 -0800

    add changes :floppy_disk:

---
+++ code/streams/transform-encrypt.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const crypto = require('crypto')
+const zlib = require('zlib')
+const SECRET = '3AABD889-25F9-4339-AAC2-3BE8EF02B047'
+
+const r = fs.createReadStream('file.txt')
+const e = crypto.createCipher('aes256', SECRET) 
+const z = zlib.createGzip()
+const w = fs.createWriteStream('file.txt.gz')
+r.pipe(e).pipe(z).pipe(w)
