6826193857c95a25d9c79f2c406fa7340232477a
Azat Mardan <hi@azat.co>
2018-01-21 15:03:07 -0800

    add changes :wc:

--- code/consoles.js
+++ code/consoles.js
@@ -1,6 +1,6 @@
 const util = require('util')
 console.log('Hello %s from %s', 'Azat', {city: 'San Francisco'}) // Hello Azat from [object Object]
-console.log('t', {city: 'San Francisco'}) // { city: 'San Francisco' }
+console.log('t', {city: 'San Francisco'}) // t { city: 'San Francisco' }
 console.log(util.inspect({city: 'San Francisco'})) // '{ city: \'San Francisco\' }'
 const str = util.inspect({city: 'San Francisco'})
 console.log(str)
