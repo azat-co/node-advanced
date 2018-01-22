464930eafabc1b5cac41c4df7f841982c36c779e
Azat Mardan <hi@azat.co>
2018-01-22 11:19:55 -0800

    add changes :six:

--- code/streams/writable-translate.js
+++ code/streams/writable-translate.js
@@ -1,17 +1,14 @@
 const {Writable} = require('stream')
 const translate = require('google-translate-api')
-
 const translateWritableStream = new Writable({
   write(chunk, encoding, callback) {
     translate(chunk.toString(), {to: 'en'}).then(res => {
-        console.log(res.text)
-        //=> I speak English
-        console.log(res.from.language.iso)
-        //=> nl
-        callback()
+      console.log('Translated text in English:', res.text)
+      console.log('Your language:', res.from.language.iso)
+      callback()
     }).catch(err => {
-        console.error(err)
-        callback()
+      console.error(err)
+      callback()
     })
   }
 })
