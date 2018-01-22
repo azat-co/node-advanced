28d02c99b7f650269c1ca2418b83e90d379093cf
Azat Mardan <hi@azat.co>
2018-01-21 16:07:21 -0800

    add changes :triangular_flag_on_post:

--- code/file-server/file-server.js
+++ code/file-server/file-server.js
@@ -1,12 +1,13 @@
 const url = require('url')
 const fs = require('fs')
+const path = require('path')
 const SECRET = process.env.SECRET
 const server = require('http').createServer((req, res) => {
   console.log(`URL is ${req.url} and the method is ${req.method}`)
   const course = req.url.match(/courses\/([0-9]*)/) // works for /courses/123 to get 123
-  const query = url.parse(req.url, true).query // works for /?key=value&key2=value2 
+  const query = url.parse(req.url, true).query // works for /?key=value&key2=value2
   if (course && course[1] && query.API_KEY === SECRET) {
-    fs.readFile('./clients_credit_card_archive.sql', (error, data)=>{
+    fs.readFile(path.join(__dirname, 'clients_credit_card_archive.sql'), (error, data)=>{
       if (error) {
         res.writeHead(500)
         res.end('Server error')
